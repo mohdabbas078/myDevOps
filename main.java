fe3cdc247ae282a0f784a9d2b8c8be9555d6742a
Abbas <abbas078@gmail.com>
2022-07-09 10:05:45 +0000

    sum of two numbers

--- main.java
+++ main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 class M
 {
 public static void main(String arg[])
 {
-System.out.println("Hello World");
+Scanner sc =new Scanner (Sytem.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c = a+b;
+System.out.println("Sum of Two Numbers = "+c);
 }
 }
